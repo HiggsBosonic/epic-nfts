96d6ca9a975fb809be370d769253df1d4b6e5729
HiggsBosonic <natstonecipher@gmail.com>
2021-09-28 09:45:53 -0400

    Update run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,20 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
-    const nftContract = await nftContractFactory.deploy();
+    /*This will actually compile our contract and generate the 
+    necessary files we need to work with our contract under the 
+    artifacts directory
+    */
+const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+/*
+This is pretty fancy :).
+What's happening here is Hardhat will create a local Ethereum network
+ for us, but just for this contract. Then, after the script completes 
+ it'll destroy that local network. So, every time you run the 
+ contract, it'll be a fresh blockchain. Whats the point? 
+ It's kinda like refreshing your local server every time so 
+ you always start from a clean slate which makes it easy to 
+ debug errors.
+*/
+const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
   };
